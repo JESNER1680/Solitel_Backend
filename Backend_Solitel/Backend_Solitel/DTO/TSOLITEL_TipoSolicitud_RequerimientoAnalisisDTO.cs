6b867e23fd991ca81daa755345cd49c9ebd5e9b3
Ernesto224 <130603932+Ernesto224@users.noreply.github.com>
2024-10-13 11:04:01 -0600

    Delete Backend_Solitel directory

--- Backend_Solitel/Backend_Solitel/DTO/TSOLITEL_TipoSolicitud_RequerimientoAnalisisDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend_Solitel.DTO
-{
-    public class TSOLITEL_TipoSolicitud_RequerimientoAnalisisDTO
-    {
-        public int TN_IdTipoSolicitud { get; set; }
-        public int TN_IdRequerimientoAnalisis { get; set; }
-    }
-
-}
