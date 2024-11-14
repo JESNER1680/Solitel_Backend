209a7332d7f4c0f4cbc0735737a74f6ba39dc9b5
HarveyMendez <harvey.mendez@ucr.ac.cr>
2024-11-13 21:31:34 -0600

    nuevo flujo de asignacion analisis

---
+++ Backend_Solitel/BW/CU/GestionarAsignacionBW.cs
@@ -0,0 +1,20 @@
+﻿using BW.Interfaces.BW;
+using BW.Interfaces.DA;
+
+namespace BW.CU
+{
+    public class GestionarAsignacionBW : IGestionarAsignacionBW
+    {
+        private readonly IGestionarAsignacionDA gestionarAsignacionDA;
+
+        public GestionarAsignacionBW(IGestionarAsignacionDA gestionarAsignacionDA)
+        {
+            this.gestionarAsignacionDA = gestionarAsignacionDA;
+        }
+
+        public async Task<bool> InsertarAsignacion(int idSolicitudAnalisis, int idUsuario)
+        {
+            return await this.gestionarAsignacionDA.InsertarAsignacion(idSolicitudAnalisis, idUsuario);
+        }
+    }
+}
